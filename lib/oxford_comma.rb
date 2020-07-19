a11aefc2e697d1130dd688ff67aa0b0b0c45e8c1
Cory Marker <cmarker4@gmail.com>
2020-07-19 06:23:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  if array.size < 2
+    array.join
+  elsif array.size < 3
+    array.join(" and ")
+  elsif array.size >= 3
+    var1 = array.pop
+    array.push("and" var1)
+    array.join(", ")
+
+  else
+    array
+  end
 
 end
